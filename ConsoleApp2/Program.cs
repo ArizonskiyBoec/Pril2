c530cdb6622274dec1a802cd10b904afad342d07
arizonskiyboec <arizonskiyboec@mail.ru>
2022-02-15 14:10:29 +0300

    Проверка Ввода

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,9 @@
 ﻿Console.WriteLine("Введите своё имя");
 string a = Console.ReadLine();
+while (a == null)
+{
+    Console.WriteLine("Вы не ввели имя.Повторите попытку");
+    a = Console.ReadLine();
+}
 Console.WriteLine("Привет "+a);
 Console.ReadKey();
